f9b3f75505477ece646a8df2682b461b164ff77b
Asirwad <asirwadsali@gmail.com>
2024-08-08 11:45:48 +0530

    add constraints to PatientDto

--- patient-service/src/main/java/com/ust/patient_service/dto/PatientDto.java
+++ patient-service/src/main/java/com/ust/patient_service/dto/PatientDto.java
@@ -3,13 +3,14 @@ package com.ust.patient_service.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
 public record PatientDto (
     int id,
 
-    @NotEmpty(message = "Name is required")
+    @NotEmpty(message = "Name is required") @Length(min = 3, max = 50, message = "Not valid name length(3 to 50)")
     String fullName,
 
     @NotEmpty(message = "Email is required")
