f8b161950244427ab3af353a84223d51f5cdaee6
Asirwad <asirwadsali@gmail.com>
2024-08-08 11:42:30 +0530

    add constraints to PatientDto

--- patient-service/src/main/java/com/ust/patient_service/dto/PatientDto.java
+++ patient-service/src/main/java/com/ust/patient_service/dto/PatientDto.java
@@ -1,12 +1,27 @@
 package com.ust.patient_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 import java.time.LocalDate;
 
 public record PatientDto (
     int id,
+
+    @NotEmpty(message = "Name is required")
     String fullName,
+
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Invalid Email")
     String email,
+
+    @NotEmpty(message = "Phone is required")
+            @Pattern(regexp = "", message = "Phone is invalid")
     String phone,
+
     LocalDate dob,
+
+    @NotEmpty(message = "Address is required")
     String address
 ){}
