305ed7a206746285bb59790cdb5cee091fea935d
Asirwad <asirwadsali@gmail.com>
2024-08-08 20:11:45 +0530

    changed DType of id from int to long and added Past constraint to dob

--- patient-service/src/main/java/com/ust/patient_service/dto/PatientDto.java
+++ patient-service/src/main/java/com/ust/patient_service/dto/PatientDto.java
@@ -2,15 +2,17 @@ package com.ust.patient_service.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
 public record PatientDto (
-    int id,
+    long id,
 
-    @NotEmpty(message = "Name is required") @Length(min = 3, max = 50, message = "Not valid name length(3 to 50)")
+    @NotEmpty(message = "Name is required")
+    @Length(min = 3, max = 50, message = "Not valid name length(3 to 50)")
     String fullName,
 
     @NotEmpty(message = "Email is required")
@@ -18,11 +20,12 @@ public record PatientDto (
     String email,
 
     @NotEmpty(message = "Phone is required")
-            @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)>\\d{10}$", message = "Phone is invalid")
+    @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)>\\d{10}$", message = "Phone is invalid")
     String phone,
 
     @NotEmpty(message = "Address is required")
     String address,
 
+    @Past(message = "Date of birth must be in past")
     LocalDate dob
 ){}
