4b71bf98cd8f76f3eac028809ecd04b2c52e4594
Asirwad <asirwadsali@gmail.com>
2024-08-08 14:36:12 +0530

    add custom methods to jpa repo

--- patient-service/src/main/java/com/ust/patient_service/repository/PatientRepo.java
+++ patient-service/src/main/java/com/ust/patient_service/repository/PatientRepo.java
@@ -3,6 +3,11 @@ package com.ust.patient_service.repository;
 import com.ust.patient_service.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientRepo extends JpaRepository<Patient, Long> {
+import java.time.LocalDate;
+import java.util.Optional;
 
+public interface PatientRepo extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+    Optional<Patient> findByPhone(String phone);
+    Optional<Patient> findByFullNameContainingAndDob(String fullName, LocalDate dob);
 }
